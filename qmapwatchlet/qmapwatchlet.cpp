c916a1e3cb9440737a9414d6f28505b67209e3cb
Javier S. Pedro <maemo@javispedro.com>
2012-01-30 19:54:04 +0100

    Minor fix

--- qmapwatchlet/qmapwatchlet.cpp
+++ qmapwatchlet/qmapwatchlet.cpp
@@ -3,7 +3,7 @@
 using namespace sowatch;
 
 QMapWatchlet::QMapWatchlet(WatchServer* server) :
-	DeclarativeWatchlet(server, "com.javispedro.sowatch.map")
+	DeclarativeWatchlet(server, "com.javispedro.sowatch.qmap")
 {
 
 	setSource(QUrl(SOWATCH_QML_DIR "/qmapwatchlet/" + server->watch()->model() + ".qml"));
